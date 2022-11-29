2e8289837ceced999a6ff5225346f61d353c4ebe
Michael Bergsten <michaelbergsten@Michaels-MacBook-Air.local>
2022-11-29 16:08:29 +0100

    new layout

--- ThreeInARow/ViewController.swift
+++ ThreeInARow/ViewController.swift
@@ -8,25 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
     @IBOutlet weak var b1: UIButton!
     @IBOutlet weak var b2: UIButton!
     @IBOutlet weak var b3: UIButton!
-    @IBOutlet weak var b4: UIButton!
-    @IBOutlet weak var b5: UIButton!
-    @IBOutlet weak var b6: UIButton!
-    @IBOutlet weak var b7: UIButton!
-    @IBOutlet weak var b8: UIButton!
-    @IBOutlet weak var b9: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func playBtn(_ sender: UIButton) {
-    }
-    
 
+    @IBAction func tapButton(_ sender: UIButton) {
+        
+        
+    }
 }
 
