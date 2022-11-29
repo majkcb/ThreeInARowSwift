2ffae9e3ff0e002876c05b62e652d17fd73e53d9
Michael Bergsten <michaelbergsten@Michaels-MacBook-Air.local>
2022-11-29 18:43:17 +0100

    fixed buttons

--- ThreeInARow/ViewController.swift
+++ ThreeInARow/ViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    enum Turn {
+        case Nought
+        case Cross
+    }
+    
+    
     @IBOutlet weak var turnLabel: UILabel!
     @IBOutlet weak var a1: UIButton!
     @IBOutlet weak var a2: UIButton!
@@ -21,16 +26,97 @@ class ViewController: UIViewController {
     @IBOutlet weak var c2: UIButton!
     @IBOutlet weak var c3: UIButton!
     
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    var board = [UIButton]()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initBoard()
     }
-
+    
+    func initBoard() {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
+    }
+    
     @IBAction func tapButton(_ sender: UIButton) {
         
+        addToBoard(sender)
+        
+        if(fullBoard()) {
+            resultAlert(title: "Draw")
+            
+        }
         
     }
+    
+    func resultAlert(title: String) {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+            
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard() {
+        
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if (firstTurn == Turn.Nought){
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+            
+        } else if (firstTurn == Turn.Cross){
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+        }
+        
+        currentTurn = firstTurn
+        
+    }
+    
+    func fullBoard() -> Bool {
+        
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func addToBoard(_ sender: UIButton) {
+        if (sender.title(for: .normal) == nil) {
+            if (currentTurn == Turn.Nought) {
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+                
+            } else if (currentTurn == Turn.Cross) {
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+                
+            }
+            
+            sender.isEnabled = false
+        }
+    }
 }
-
